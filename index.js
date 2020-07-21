aa1ad846d40181b40cb302c52e13882599d41c58
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-21 02:47:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay (musicians,instruments) {
+  var musiciansPlays = []
+  for (let i = 0; i < musicians.length; i++) {
+    musiciansPlays.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+  return musiciansPlays
+}
+
+function johnLennonFacts (facts) {
+  var excitingJohnLennonFacts = []
+  while (i < facts.length){
+    excitingJohnLennonFacts.push("${facts[i]}!!!")
+
+    facts++;
+  }
+  return excitingJohnLennonFacts
+}
